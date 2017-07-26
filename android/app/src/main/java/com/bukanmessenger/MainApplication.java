4ff6793fe2ccd48ff3886f53d09e5b23102ac20e
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-07-26 10:10:02 +0700

    rn-maps android setting

--- android/app/src/main/java/com/bukanmessenger/MainApplication.java
+++ android/app/src/main/java/com/bukanmessenger/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +26,7 @@ public class MainApplication extends NavigationApplication implements ReactAppli
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           // new MainReactPackage()
+          new MapsPackage()
       );
     }
   };
